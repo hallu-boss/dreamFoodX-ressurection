8abb3196a8aa88220a5285d0384fe768a1d3a7e7
Paweł Hase <pawelhase@gmail.com>
2025-10-02 21:45:11 +0200

    Test fix

--- backend/tests/base.test.ts
+++ backend/tests/base.test.ts
@@ -1,5 +1,6 @@
 import request from "supertest";
-import app from "../src/server";
+import { describe, it, expect, beforeAll, afterAll } from "@jest/globals";
+import { app } from "../src/app";
 
 describe("GET / - status message", () => {
     it("should return 201", async () => {
