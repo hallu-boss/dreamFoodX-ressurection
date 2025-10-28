7e04b69a224e681a0d1a6c8cda31492c6fd89df1
Erneo97 <michalkaniewski1997@gmail.com>
2025-10-28 11:39:46 +0100

    Dodanie pól w formularzu rejestracji: imię, nzaweisko.
    Obsługa rejestracji z bazą danych i możliwych błędów

--- frontend/app/src/main/java/com/example/frontend/ui/screens/RegisterScreen.kt
+++ frontend/app/src/main/java/com/example/frontend/ui/screens/RegisterScreen.kt
@@ -12,26 +12,34 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import com.example.firstcomposeap.ui.navigation.main.Screen
 import com.example.frontend.ui.components.FullSizeButton
 import com.example.frontend.ui.components.InputField
 import com.example.frontend.ui.components.PasswordInputField
 import com.example.frontend.ui.components.validateEmail
+import com.example.frontend.ui.service.RegisterViewModel
 
 
 @Composable
-fun RegisterScreen(navController: NavHostController) {
+fun RegisterScreen(navController: NavHostController,
+                   viewModel: RegisterViewModel = viewModel()) {
     val context = LocalContext.current
+    var name by remember { mutableStateOf("") }
+    var surname by remember { mutableStateOf("") }
     var email by remember { mutableStateOf("") }
     var isError by remember { mutableStateOf(false) }
     var password by remember { mutableStateOf("") }
     var confirmPassword by remember { mutableStateOf("") }
     var isPasswordError by remember { mutableStateOf(false) }
+    val registerState = viewModel.registerState
+    val errorMessage = viewModel.errorMessage
 
 
     Box(
@@ -47,6 +55,18 @@ fun RegisterScreen(navController: NavHostController) {
         ) {
 
             Text(text = "Rejestracja", fontSize = 38.sp, fontWeight = FontWeight.Bold)
+            Spacer(modifier = Modifier.height(25.dp))
+            InputField(
+                value = name,
+                onValueChange = {  name = it },
+                label = "Imię"
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+            InputField(
+                value = surname,
+                onValueChange = {  surname = it },
+                label = "Nazwisko"
+            )
             Spacer(modifier = Modifier.height(16.dp))
             InputField(value = email,
                 isError = isError,
@@ -83,18 +103,23 @@ fun RegisterScreen(navController: NavHostController) {
             FullSizeButton(
                 text = "Zarejestruj się",
                 onClick = {
-                /* TODO obsługa Rejestracji */
-                    navController.navigate(Screen.Home.route)
+
+
+                    viewModel.registerUser(name, surname, email, password, 0)
+
                     if( isError || email.isEmpty() ) {
                         Toast.makeText(context, "Email musi być w formie example@example.pl", Toast.LENGTH_LONG).show()
                     }
                     else if( isPasswordError || password.isEmpty() || confirmPassword.isEmpty()) {
                         Toast.makeText(context, "Hasła muszą być takie same", Toast.LENGTH_LONG).show()
                     }
-                    else {
-
+                    else  if ( name.isNotEmpty() && surname.isNotEmpty()) {
+                        viewModel.registerUser(name, surname, email, password, 0)
                     }
-
+                    else {
+                        Toast.makeText(context, "Nie wszystkie pola zostały wypełnione", Toast.LENGTH_LONG)
+                            .show()
+                    }
                 }
             )
             Spacer(modifier = Modifier.height(16.dp))
@@ -103,7 +128,20 @@ fun RegisterScreen(navController: NavHostController) {
                 onClick = {  navController.navigate(Screen.Login.route) }
             )
 
+            registerState?.let { response ->
+                LaunchedEffect(response) {
+                    Toast.makeText(context, "Rejestracja przebiegła pomyślnie", Toast.LENGTH_LONG).show()
+                    navController.navigate(Screen.Login.route) {
+                        popUpTo(Screen.Register.route) { inclusive = true }
+                    }
+                }
+            }
 
+            errorMessage?.let { error ->
+                LaunchedEffect(error) {
+                    Toast.makeText(context, error, Toast.LENGTH_LONG).show()
+                }
+            }
 
 
         }
