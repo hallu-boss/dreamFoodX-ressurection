fc4d1271f87ee8f27ad9f6b51bdb3120ea6fe297
Erneo97 <michalkaniewski1997@gmail.com>
2025-10-28 14:11:49 +0100

    Edycja ikon w menu po zalogowaniu

--- frontend/app/src/main/java/com/example/frontend/ui/navigation/BottomMenu.kt
+++ frontend/app/src/main/java/com/example/frontend/ui/navigation/BottomMenu.kt
@@ -1,18 +1,15 @@
 package com.example.firstcomposeap.ui.navigation.main
 
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Build
-import androidx.compose.material.icons.filled.DateRange
-import androidx.compose.material.icons.filled.Favorite
-import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
-
+import com.example.frontend.R
 
 
 @Composable
@@ -20,17 +17,29 @@ fun BottomMenu(
     selectedItem: String,
     onItemSelected: (String) -> Unit
 ) {
+    val context = LocalContext.current
     val items = listOf(
-        MenuItem("Strona główna", Icons.Default.Build),
-        MenuItem("Koszyk", Icons.Default.Favorite),
-        MenuItem("Moje przepisy", Icons.Default.Person),
-        MenuItem("Konto", Icons.Default.DateRange)
+        MenuItem("Strona główna", drawableRes = R.drawable.meal),
+        MenuItem("Koszyk", drawableRes = R.drawable.shopping),
+        MenuItem("Moje przepisy", drawableRes = R.drawable.burger),
+        MenuItem("Konto", drawableRes = R.drawable.profile)
     )
 
     NavigationBar(tonalElevation = 8.dp) {
         items.forEach { item ->
             NavigationBarItem(
-                icon = { Icon(item.icon, contentDescription = item.title) },
+                icon = {
+                    when {
+                        item.imageVector != null -> Icon(
+                            imageVector = item.imageVector,
+                            contentDescription = item.title
+                        )
+                        item.drawableRes != null -> Icon(
+                            painter = painterResource(id = item.drawableRes),
+                            contentDescription = item.title
+                        )
+                    }
+                },
                 label = { Text(item.title) },
                 selected = selectedItem == item.title,
                 onClick = { onItemSelected(item.title) }
@@ -39,4 +48,9 @@ fun BottomMenu(
     }
 }
 
-data class MenuItem(val title: String, val icon: ImageVector)
+
+data class MenuItem(
+    val title: String,
+    val imageVector: ImageVector? = null,
+    val drawableRes: Int? = null
+)
