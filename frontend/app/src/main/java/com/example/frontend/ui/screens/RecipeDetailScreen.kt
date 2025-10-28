b21d8bcadf08736bffaa850d4e89254b0d1cb628
Erneo97 <michalkaniewski1997@gmail.com>
2025-10-28 16:37:29 +0100

    Strona z szczegółami klikniętego przepisu ze strony głownej

---
+++ frontend/app/src/main/java/com/example/frontend/ui/screens/RecipeDetailScreen.kt
@@ -0,0 +1,94 @@
+package com.example.frontend.ui.screens
+
+import android.widget.Space
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
+import com.example.firstcomposeap.ui.navigation.main.MainLayout
+import com.example.frontend.ui.service.RecipeViewModel
+
+@Composable
+fun RecipeDetailScreen(recipeId: String,
+                       viewModel: RecipeViewModel, navController: NavHostController) {
+    var selectedItem by remember { mutableStateOf("Strona główna") }
+
+
+    MainLayout(
+        navController = navController,
+        selectedItem = selectedItem,
+        onItemSelected = { selectedItem = it }
+    ) { innerPadding ->
+        Box(
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            Box(
+                modifier = Modifier
+                    .padding(8.dp)
+                    .fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(16.dp),
+                    verticalArrangement = Arrangement.Top,
+                    horizontalAlignment = Alignment.Start
+                ) {
+                    Row (modifier = Modifier.fillMaxWidth()
+                    ){
+                        IconButton(onClick = { navController.popBackStack() }) {
+                            Icon(
+                                imageVector = Icons.Default.ArrowBack,
+                                contentDescription = "Cofnij",
+                                tint = Color.White,
+                                modifier = Modifier.size(28.dp)
+                            )
+                        }
+                        Spacer(modifier = Modifier.width(20.dp))
+                        Text("Szczegóły ")
+                    }
+
+
+                    Spacer(modifier = Modifier.height(16.dp))
+
+
+                    Column(
+                        modifier = Modifier.fillMaxSize(),
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Text("Id wybranego przepisu: $recipeId", color = Color.White)
+                    }
+                }
+            }
+        }
+    }
+}
