63b1ef10e490318563fcdd400b448f4dadd44da1
Erneo97 <michalkaniewski1997@gmail.com>
2025-10-28 11:38:23 +0100

    doinstalowanie paczek do obsługi komunikacji sieciowej

--- frontend/app/build.gradle.kts
+++ frontend/app/build.gradle.kts
@@ -41,6 +41,10 @@ android {
 
 dependencies {
     implementation("androidx.navigation:navigation-compose:2.7.0")
+    implementation("io.coil-kt:coil-compose:2.5.0")
+    implementation("com.squareup.retrofit2:retrofit:2.11.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.11.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
