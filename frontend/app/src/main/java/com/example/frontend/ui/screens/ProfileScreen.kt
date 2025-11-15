82b4e189658e81e4124d5520e16f6d0afe9c8390
Erneo97 <michalkaniewski1997@gmail.com>
2025-11-15 22:50:30 +0100

    Dodanie komponentów do zmiany hasła użytkownika

--- frontend/app/src/main/java/com/example/frontend/ui/screens/ProfileScreen.kt
+++ frontend/app/src/main/java/com/example/frontend/ui/screens/ProfileScreen.kt
@@ -1,6 +1,7 @@
 package com.example.frontend.ui.screens
 
 
+import android.widget.Toast
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -13,10 +14,12 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import com.example.firstcomposeap.ui.components.UniversalEditCard
+import com.example.firstcomposeap.ui.components.profile.profileTab.ChangePasswordDialog
 import com.example.firstcomposeap.ui.components.profile.profileTab.EditUserDialog
 import com.example.firstcomposeap.ui.navigation.main.MainLayout
 import com.example.frontend.ui.service.LoginViewModel
@@ -25,14 +28,19 @@ import com.example.frontend.ui.service.LoginViewModel
 fun ProfileScreen(navController: NavHostController,
                   userViewModel: LoginViewModel
 ) {
+    val context = LocalContext.current
     var selectedItem by remember { mutableStateOf("Konto") }
     userViewModel.downloadUserProfile()
+
+    val message = userViewModel.errorMessage
+    val errorMessage = userViewModel.succesfulMessage
 
     var userProfile = userViewModel.userProfile
     LaunchedEffect(userViewModel.userProfile) {
         userProfile = userViewModel.userProfile
     }
     var showDialog by remember { mutableStateOf(false) }
+    var showPasswordDialog by remember { mutableStateOf(false) }
 
     MainLayout(
         navController = navController,
@@ -57,6 +65,14 @@ fun ProfileScreen(navController: NavHostController,
 
                     },
                     onClick = { showDialog = true }
+                )
+
+                UniversalEditCard(
+                    data = {
+                        Text("Zmień stwoje hasło", fontSize = 30.sp)
+
+                    },
+                    onClick = { showPasswordDialog = true }
                 )
             }
             else {
@@ -88,4 +104,29 @@ fun ProfileScreen(navController: NavHostController,
             }
         )
     }
+
+    if (showPasswordDialog) {
+        ChangePasswordDialog(
+            onDismiss = { showPasswordDialog = false },
+            onConfirm = { oldPassword, newPassword  ->
+//               TODO: userViewModel.updatePassword(oldPassword, newPassword)
+                showPasswordDialog = false
+            }
+        )
+    }
+
+    LaunchedEffect(message) {
+        if (message?.contains("Zmieniono", ignoreCase = true) == true) {
+            showDialog = false
+            Toast.makeText(context, "Zmieniono hasło pomyślnie", Toast.LENGTH_SHORT).show()
+            userViewModel.succesfulMessage = null
+        }
+    }
+
+    LaunchedEffect(errorMessage) {
+        if (errorMessage != null) {
+            Toast.makeText(context, errorMessage, Toast.LENGTH_SHORT).show()
+        }
+    }
+
 }
