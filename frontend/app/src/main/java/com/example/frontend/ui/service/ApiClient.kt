111f57ab3ddf77f23640143912d1c07b3e3226a2
Erneo97 <michalkaniewski1997@gmail.com>
2025-11-27 11:10:17 +0100

    użycie url serwera do komunikacji

--- frontend/app/src/main/java/com/example/frontend/ui/service/ApiClient.kt
+++ frontend/app/src/main/java/com/example/frontend/ui/service/ApiClient.kt
@@ -6,11 +6,11 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object ApiClient {
-    private const val BASE_URL = "https://d6e2a4bc6e0d.ngrok-free.app/api/"
+//    private const val BASE_URL = "https://7e510bc1e283.ngrok-free.app/api/"
     // IP danego komputera w scieci lokalnej  Należy zmienic też w network_security_config
 
     // 10.0.2.2 to adres hosta komputera gdzie działa emulator
-    //private const val BASE_URL = "http://10.0.2.2:5000/api/"
+    private const val BASE_URL = "http://10.0.2.2:5000/api/"
 
     // Przy lgowaniu się z innej sieci używać z ngroka (przy logowaniu przez googla lub faceboka jest to konieczne)
     // ngrok http 5000
