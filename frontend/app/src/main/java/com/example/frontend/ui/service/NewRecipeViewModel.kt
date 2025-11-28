bc4c736d1778a8b68da9dc0c75b620afea07eca3
Erneo97 <michalkaniewski1997@gmail.com>
2025-11-28 11:04:01 +0100

    stałe do tworzenia przepisu

--- frontend/app/src/main/java/com/example/frontend/ui/service/NewRecipeViewModel.kt
+++ frontend/app/src/main/java/com/example/frontend/ui/service/NewRecipeViewModel.kt
@@ -1,12 +1,22 @@
 package com.example.frontend.ui.service
 
+import android.net.Uri
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 
 class NewRecipeViewModel : ViewModel() {
+    val categories = listOf("wszystkie", "przekąska", "obiad", "sniadanie", "dodatek", "napój")
+
+//    Zmienne z informacji
 
     var nazwa by mutableStateOf("")
+    var kategoria by mutableStateOf(false)
+    var czyPubliczny by mutableStateOf(false)
+    var cena by mutableStateOf(0.0f)
+
+    var obraz by mutableStateOf<Uri?>(null)
+
 
 }
