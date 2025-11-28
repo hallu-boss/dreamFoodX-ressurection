f559c5ea77a3f4e9c246bdcfe33f43436c3c35b1
Erneo97 <michalkaniewski1997@gmail.com>
2025-11-28 15:15:34 +0100

    metoda do sprawdzania czy pola nazwa, kategoria i czy dodano zdjęcie.
    lista wszystkich skłądkikó dostępnych dla uzytownika

--- frontend/app/src/main/java/com/example/frontend/ui/service/NewRecipeViewModel.kt
+++ frontend/app/src/main/java/com/example/frontend/ui/service/NewRecipeViewModel.kt
@@ -2,6 +2,7 @@ package com.example.frontend.ui.service
 
 import android.net.Uri
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
@@ -12,11 +13,22 @@ class NewRecipeViewModel : ViewModel() {
 //    Zmienne z informacji
 
     var nazwa by mutableStateOf("")
-    var kategoria by mutableStateOf(false)
+    var kategoria by mutableStateOf("")
     var czyPubliczny by mutableStateOf(false)
     var cena by mutableStateOf(0.0f)
 
     var obraz by mutableStateOf<Uri?>(null)
+
+    fun validateBasicInformation( ) : Boolean {
+        if( nazwa=="" || kategoria=="" || obraz == null)
+            return false
+        return true
+    }
+
+//    Zmienne składników
+    val utilsList = listOf("g", "ml", "szt")
+    val ingredientCategoryList = listOf("Mięso", "Przyprawy", "Warzywa", "Owoce", "Nabiał","Słodycze", "Produkty zbożowe", "Tłuszcze", "Woda", "Orzechy", "Dodatki")
+    var userIngredientsList = mutableStateListOf<Ingredient>()
 
 
 }
